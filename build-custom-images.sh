0a797cdd7e0e673954a9888dbca5a4ee9a9f6bf1
ItsMurumba <kevmurumba@gmail.com>
2024-05-21 17:33:41 +0300

    Adds Custom Image build script and Kibana cleanup

---
+++ build-custom-images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker build \
+    -t itechuw/opencr:local \
+    -f packages/client-registry-opencr/Dockerfile \
+    packages/client-registry-opencr \
+    --no-cache
+
+# Build a Botswana Specific ElasticSearch instance
+# TEMP: Ensure the /tmp/backups folder exists
+mkdir /tmp/backup
+
+docker build \
+    -t docker.elastic.co/elasticsearch/elasticsearch:local \
+    -f packages/analytics-datastore-elastic-search/Dockerfile \
+    packages/analytics-datastore-elastic-search \
+    --no-cache
+
+# Build the Platform to contain the above custom builds
+./build-image.sh
+
+echo "You can run the the Platform commands: E.g: ./instant-linux package init -p dev"
