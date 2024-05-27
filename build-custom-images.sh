e1ff72a209ea351bb3a740ceb43ddeb95d44ea6f
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2024-05-27 16:02:55 +0100

    feat: create a pkg for omang svc

--- build-custom-images.sh
+++ build-custom-images.sh
@@ -16,6 +16,12 @@ docker build \
     packages/analytics-datastore-elastic-search \
     --no-cache
 
+docker build \
+    -t jembi/omangsvc:latest \
+    -f projects/omang-service-mediator/Dockerfile \
+    projects/omang-service-mediator/ \
+    --no-cache
+
 # Build the Platform to contain the above custom builds
 ./build-image.sh
 
