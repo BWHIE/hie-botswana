dc43235730f4df34c2bb53dc99f98a0a2d2bc943
Martin Brocker <brocker.martin@gmail.com>
2024-06-03 08:27:08 +0200

    Remove the custom module build steps
    These custom modules no longer need to be manually built as they have been uploaded to the repository for use
    
    CU_86byrk7jf

--- projects/openmrs/build-openmrs.sh
+++ projects/openmrs/build-openmrs.sh
@@ -29,55 +29,7 @@ else
     exit 1
 fi
 
-# =========== Remove any existing commits for submodules - Ensure repo is latest ==============
-
-(
-    cd openmrs-module-botswanaemr-reports &> /dev/null && 
-    git fetch --all &> /dev/null && git reset --hard origin/master &> /dev/null && 
-    reports=$(mvn clean install 2>&1 | grep -oP '(?<=Reactor Summary for Botswanaemr Reports )\d+\.\d+\.\d+(-SNAPSHOT)?')
-    echo "Updated module version 'openmrs-module-botswanaemr-reports': $reports"
-)
-
-(
-    cd openmrs-module-botswanaemr.inventory &> /dev/null && 
-    git fetch --all &> /dev/null && git reset --hard origin/main &> /dev/null && 
-    inventory=$(mvn clean install 2>&1 | grep -oP '(?<=Reactor Summary for Botswana Inventory Module )\d+\.\d+\.\d+(-SNAPSHOT)?')
-    echo "Updated module version 'openmrs-module-botswanaemr.inventory': $inventory"
-)
-
-(
-    cd openmrs-module-labonfhir &> /dev/null && 
-    git fetch --all &> /dev/null && git reset --hard origin/main &> /dev/null && 
-    labonfhir=$(mvn clean install 2>&1 | grep -oP '(?<=Reactor Summary for Lab on FHIR )\d+\.\d+\.\d+(-SNAPSHOT)?')
-    echo "Updated module version 'openmrs-module-labonfhir': $labonfhir"
-)
-
-(
-    cd openmrs-module-locationbasedaccess &> /dev/null && 
-    git fetch --all &> /dev/null && git reset --hard origin/master &> /dev/null && 
-    locationbasedaccess=$(mvn clean install 2>&1 | grep -oP '(?<=Reactor Summary for Location Based Access Control )\d+\.\d+\.\d+(-SNAPSHOT)?')
-    echo "Updated module version 'openmrs-module-locationbasedaccess': $locationbasedaccess"
-)
-
-(
-    cd openmrs-module-patientqueueing &> /dev/null && 
-    git fetch --all &> /dev/null && git reset --hard origin/master &> /dev/null && 
-    patientqueueing=$(mvn clean install 2>&1 | grep -oP '(?<=Reactor Summary for Patient Queueing )\d+\.\d+\.\d+(-SNAPSHOT)?')
-    echo "Updated module version 'openmrs-module-patientqueueing': $patientqueueing"
-)
-
-# =========== Prompt to confirm manual changes have been done ==============
-
-while true; do
-    read -p "Please update the 'openmrs-module-botswanaemr' -> 'openmrs-distro.properties' with the above updated module versions? [y/n]" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-# =========== Run Botswana EMR build steps - Docker output ==============
+# =========== Build the new distro / docker files ==============
 
 # Change directory and build OpenMRS distro
 cd openmrs-module-botswanaemr || { 
@@ -87,11 +39,5 @@ cd openmrs-module-botswanaemr || {
 
 # remove the existing folder to ensure no create conflicts exists
 rm -r ~/openmrs/botswanaemr
-
-mvn openmrs-sdk:setup -DserverId=botswanaemr -Ddistro=referenceapplication:2.12.2 -DbatchAnswers="8080,1044,MySQL 5.6 in SDK docker container (requires pre-installed Docker)"
-
-mvn openmrs-sdk:deploy -DserverId=botswanaemr -Dplatform=2.5.0
-
-cp -r configuration ~/openmrs/botswanaemr/configuration
 
 mvn openmrs-sdk:build-distro -DdbSql=./db/initial_db.sql -Ddir=docker -Dreset
