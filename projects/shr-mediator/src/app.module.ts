ac0aa46db98bf8ca1058d16142397fb5d0d885a5
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2024-07-03 09:49:36 +0300

    fix: logger service

--- projects/shr-mediator/src/app.module.ts
+++ projects/shr-mediator/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { BotswanaModule } from './botswana/botswana.module';
 import { OpenHimModule } from './openhim/openhim.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [CommonModule, BotswanaModule, OpenHimModule],
+  imports: [CommonModule, BotswanaModule, OpenHimModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
