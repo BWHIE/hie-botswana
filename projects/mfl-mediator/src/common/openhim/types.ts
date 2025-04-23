bbd99c603ac0f78d3c948792cc5de832868ed814
ItsMurumba <kevmurumba@gmail.com>
2025-04-23 16:50:56 +0300

    feat(openhim): add OpenHIM types for type safety

--- projects/mfl-mediator/src/common/openhim/types.ts
+++ projects/mfl-mediator/src/common/openhim/types.ts
@@ -45,3 +45,66 @@ export interface OpenHIMTransactionResponse {
   status: string;
   response: OpenHIMResponse;
 }
+
+export interface OpenHimConfig {
+  username: string;
+  password: string;
+  apiURL: string;
+  trustSelfSigned: boolean;
+  rejectUnauthorized: boolean;
+  urn: string;
+}
+
+export interface MediatorConfig {
+  urn: string;
+  version: string;
+  name: string;
+  description: string;
+  defaultChannelConfig: any[];
+  endpoints: any[];
+}
+
+export interface OpenHimHeaders {
+  [key: string]: string;
+}
+
+export type OpenHimStatus = "Success" | "Failed" | "Processing";
+
+export interface OpenHimOrchestration {
+  name: string;
+  request: {
+    path: string;
+    headers: OpenHimHeaders;
+    querystring: string;
+    body: any;
+    method: string;
+    timestamp: string;
+  };
+  response: {
+    status: number;
+    headers: OpenHimHeaders;
+    body: any;
+    timestamp: string;
+  };
+}
+
+export interface OpenHimError {
+  message: string;
+  stack?: string;
+}
+
+export const OPENHIM_TRANSACTION_ID = "x-openhim-transactionid";
+
+export type OpenHimResponse = {
+  "x-mediator-urn": string;
+  status: OpenHimStatus;
+  response: {
+    status: number;
+    headers: OpenHimHeaders;
+    body: string;
+    timestamp: Date;
+  };
+  orchestrations?: OpenHimOrchestration[];
+  properties?: Record<string, string>;
+  error?: OpenHimError;
+};
