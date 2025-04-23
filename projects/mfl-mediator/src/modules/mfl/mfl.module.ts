9d0639d93c9fb48116f59f1c638df56c7d0fd8b0
ItsMurumba <kevmurumba@gmail.com>
2025-04-23 16:46:12 +0300

    feat(mfl-mediator): update MflModule to provide ApiService

--- projects/mfl-mediator/src/modules/mfl/mfl.module.ts
+++ projects/mfl-mediator/src/modules/mfl/mfl.module.ts
@@ -4,11 +4,13 @@ import { MflController } from "./controllers/mfl.controller";
 import { OpenhimModule } from "../../common/openhim/openhim.module";
 import { HttpModule } from "@nestjs/axios";
 import { ConfigModule } from "@nestjs/config";
+import { TransactionService } from "../../common/openhim/transaction/transaction.service";
+import { ApiService } from "./services/api.service";
 
 @Module({
   imports: [HttpModule, ConfigModule, OpenhimModule],
   controllers: [MflController],
-  providers: [MflService],
+  providers: [MflService, TransactionService, ApiService],
   exports: [MflService],
 })
 export class MflModule {}
