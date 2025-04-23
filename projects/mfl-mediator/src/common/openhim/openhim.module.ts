a8b76c6ebc62b775832516ebb6280ee95caab587
ItsMurumba <kevmurumba@gmail.com>
2025-04-23 16:51:26 +0300

    feat(openhim): update OpenHIM module configuration

--- projects/mfl-mediator/src/common/openhim/openhim.module.ts
+++ projects/mfl-mediator/src/common/openhim/openhim.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { OpenhimService } from "./openhim.service";
 import { ConfigModule } from "@nestjs/config";
 import { TransactionService } from "./transaction/transaction.service";
+import { OpenhimRegistrationService } from "./openhim-registration.service";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [ConfigModule],
-  providers: [OpenhimService, TransactionService],
-  exports: [OpenhimService, TransactionService],
+  imports: [ConfigModule, HttpModule],
+  providers: [OpenhimService, TransactionService, OpenhimRegistrationService],
+  exports: [OpenhimService, TransactionService, OpenhimRegistrationService],
 })
 export class OpenhimModule {}
