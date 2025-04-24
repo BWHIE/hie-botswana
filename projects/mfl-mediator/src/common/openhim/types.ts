2f4dca3962165045f72c876a39ae6f16591b2809
ItsMurumba <kevmurumba@gmail.com>
2025-04-24 13:59:42 +0300

    docs: add comprehensive documentation to MFL mediator

--- projects/mfl-mediator/src/common/openhim/types.ts
+++ projects/mfl-mediator/src/common/openhim/types.ts
@@ -1,33 +1,34 @@
 export interface OpenHIMTransaction {
-  _id: string;
-  status: string;
-  response: {
-    status: number;
-    headers: Record<string, string>;
-    body: string;
-  };
+  status: OpenHimStatus;
   request: {
     path: string;
     querystring: string;
-    headers: Record<string, string>;
+    headers: OpenHimHeaders;
+    body: string;
+    timestamp: string;
+  };
+  response: {
+    status: number;
+    headers: OpenHimHeaders;
     body: string;
   };
   clientID: string;
   route: string;
   startedAt: string;
   finishedAt: string;
+  properties?: Record<string, string>;
 }
 
 export interface OpenHIMResponse {
   status: number;
-  headers: Record<string, string>;
+  headers: OpenHimHeaders;
   body: string;
 }
 
 export interface OpenHIMRequest {
   path: string;
   querystring: string;
-  headers: Record<string, string>;
+  headers: OpenHimHeaders;
   body: string;
 }
 
@@ -46,65 +47,95 @@ export interface OpenHIMTransactionResponse {
   response: OpenHIMResponse;
 }
 
-export interface OpenHimConfig {
-  username: string;
-  password: string;
-  apiURL: string;
-  trustSelfSigned: boolean;
-  rejectUnauthorized: boolean;
-  urn: string;
-}
-
 export interface MediatorConfig {
   urn: string;
   version: string;
   name: string;
   description: string;
-  defaultChannelConfig: any[];
-  endpoints: any[];
+  defaultChannelConfig: DefaultChannelConfig[];
+  endpoints: Endpoint[];
+  configDefs: any[];
 }
 
-export interface OpenHimHeaders {
-  [key: string]: string;
-}
-
-export type OpenHimStatus = "Success" | "Failed" | "Processing";
-
-export interface OpenHimOrchestration {
+export interface DefaultChannelConfig {
   name: string;
-  request: {
-    path: string;
-    headers: OpenHimHeaders;
-    querystring: string;
-    body: any;
-    method: string;
-    timestamp: string;
-  };
-  response: {
-    status: number;
-    headers: OpenHimHeaders;
-    body: any;
-    timestamp: string;
-  };
+  urlPattern: string;
+  routes: Route[];
+  allow: string[];
+  methods: string[];
+  type: string;
 }
 
-export interface OpenHimError {
+export interface Route {
+  name: string;
+  host: string;
+  port: string;
+  path: string;
+  primary: boolean;
+  type: string;
+}
+
+export interface Endpoint {
+  name: string;
+  host: string;
+  port: string;
+  path: string;
+  primary: boolean;
+  type: string;
+}
+
+export interface OpenHimConfig {
+  username: string;
+  password: string;
+  trustSelfSigned: boolean;
+  apiURL: string;
+  rejectUnauthorized: boolean;
+  urn: string;
+}
+
+export type OpenHimHeaders = Record<string, string>;
+
+export type OpenHimRequestDefinition = {
+  path: string;
+  headers: OpenHimHeaders;
+  querystring: string;
+  body: string;
+  method: string;
+  timestamp: string;
+};
+
+export type OpenHimResponseDefinition = {
+  status: number;
+  headers: OpenHimHeaders;
+  body: string;
+  timestamp: string;
+};
+
+export type OpenHimStatus =
+  | "Processing"
+  | "Failed"
+  | "Completed"
+  | "Successful"
+  | "Completed with error(s)";
+
+export type OpenHimOrchestration = {
+  name: string;
+  request: OpenHimRequestDefinition;
+  response?: OpenHimResponseDefinition;
+};
+
+export type OpenHimError = {
   message: string;
-  stack?: string;
-}
-
-export const OPENHIM_TRANSACTION_ID = "x-openhim-transactionid";
+  stack: string;
+};
 
 export type OpenHimResponse = {
   "x-mediator-urn": string;
   status: OpenHimStatus;
-  response: {
-    status: number;
-    headers: OpenHimHeaders;
-    body: string;
-    timestamp: Date;
-  };
+  response: OpenHimResponseDefinition;
   orchestrations?: OpenHimOrchestration[];
   properties?: Record<string, string>;
   error?: OpenHimError;
 };
+
+export const OPENHIM_TRANSACTION_ID = "x-openhim-transactionid";
